6c53b3c2e31d519b0cc9304c90a43e77b70dd767
dassaperez <dassaperez@gmail.com>
2020-05-22 22:23:44 -0400

    update lesson5 js

--- lesson5/js/main5.js
+++ lesson5/js/main5.js
@@ -43,3 +43,21 @@ document.getElementById("year").textContent = year;
 // let time = date.toLocaleString('en-US');
 
 // document.getElementById("updating").textContent = time;
+
+/*-------------------Week 5---------------*/
+
+/* Current Date Code */
+
+const options = {weekday: 'long', day: 'numeric', month: 'long', year: 'numeric'};
+ document.getElementById('date').textContent = new Date().toLocaleDateString('en-US', options);
+
+/* Displaying on Friday only */
+
+let day = new Date();
+const banner = document.getElementById('banner');
+if(day.getDay() == 5) {
+    banner.style.display = "block";
+}
+else {
+    banner.style.display = "none";
+}
